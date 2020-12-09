68265cf4ba8d98b07643dc120dbc3ede0764b9fc
Aleksey Konstantinov <alex_kav@mail.ru>
2020-12-09 14:12:59 +0300

    Урок 14 2 часть

---
+++ dz_14_urok_chast1/script.js
@@ -0,0 +1,36 @@
+
+function DomElement(selector, height, width, background_bg, fontSize){
+    this.selector =  selector;
+    this.height =  height;
+    this.width = width;
+    this.background_bg = background_bg;
+    this.fontSize = fontSize;
+}
+
+DomElement.prototype.select = function(){
+    if (this.selector.substring(0, 1) === '.') {
+        let div = document.createElement('div');
+            div.className = this.selector;
+            div.innerHTML = 'Создали div с классом';
+            document.querySelector('body').append(div);
+            div.style.cssText = 'height: ' + this.height +'px; width: ' + this.width + 'px; background-color: ' + this.background_bg + '; font-size: ' + this.fontSize + 'px;';
+            
+    }
+    if (this.selector.substring(0, 1) === '#') {
+        let div = document.createElement('p');
+            div.id = this.selector;
+            div.innerHTML = 'Создали параграф с id';
+            document.querySelector('body').append(div);
+            div.style.cssText = 'height: ' + this.height +'px; width: ' + this.width + 'px; background-color: ' + this.background_bg + '; font-size: ' + this.fontSize + 'px;';
+    }
+}
+
+DomElement.prototype.style = function () {
+
+}
+
+let domElement1 = new DomElement('.des', 100, 100, 'red', 28),
+    domElement2 = new DomElement('#des', 30, 300, 'green', 20);
+
+domElement1.select();
+domElement2.select();

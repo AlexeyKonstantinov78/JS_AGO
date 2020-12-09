440d6455abef073f88a586197d6aa0b8834137d6
Aleksey Konstantinov <alex_kav@mail.ru>
2020-12-09 19:46:19 +0300

    урок 14 1 задание fix

--- dz_14_urok_chast1/script.js
+++ dz_14_urok_chast1/script.js
@@ -1,4 +1,15 @@
 
+let stroka,
+    color = 'red';
+
+for( ; ; ){
+    stroka = String(prompt('ввежите . или #')); 
+    if (stroka === '.' || stroka === '#') {
+        if(stroka === '#') {color = 'green'}
+        break;
+    } else { alert('ошибка значения ');}
+}
+    
 function DomElement(selector, height, width, background_bg, fontSize){
     this.selector =  selector;
     this.height =  height;
@@ -17,16 +28,15 @@ DomElement.prototype.select = function(){
             
     }
     if (this.selector.substring(0, 1) === '#') {
-        let div = document.createElement('p');
-            div.id = this.selector;
-            div.innerHTML = 'Создали параграф с id';
-            document.querySelector('body').append(div);
-            div.style.cssText = 'height: ' + this.height +'px; width: ' + this.width + 'px; background-color: ' + this.background_bg + '; font-size: ' + this.fontSize + 'px;';
+        let p = document.createElement('p');
+            p.id = this.selector;
+            p.innerHTML = 'Создали параграф с id';
+            document.querySelector('body').append(p);
+            p.style.cssText = 'height: ' + this.height +'px; width: ' + this.width + 'px; background-color: ' + this.background_bg + '; font-size: ' + this.fontSize + 'px;';
     }
 }
 
-let domElement1 = new DomElement('.des', 100, 100, 'red', 28),
-    domElement2 = new DomElement('#des', 30, 300, 'green', 20);
-
-domElement1.select();
-domElement2.select();
+    domElement = new DomElement(stroka+'createdId', 100, 100, color, 28);
+    
+    domElement.select();    
+ 

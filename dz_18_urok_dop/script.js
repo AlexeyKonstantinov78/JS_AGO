f864d9f81dab55bc4234b83f722b7ba8297c5f65
Aleksey Konstantinov <alex_kav@mail.ru>
2020-12-14 14:35:07 +0300

    урок 18 дополнительно

--- dz_18_urok_dop/script.js
+++ dz_18_urok_dop/script.js
@@ -1,2 +1,27 @@
 'ude strict';
 
+const timeDays = document.querySelector('#time-days'),
+    week = document.querySelector('#week'),
+    time = document.querySelector('#time'),
+    timeKol = document.querySelector('#time-kol'),
+    weekTime = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    date = new Date(),    
+    day = date.getDay(),
+    count = Math.floor(((new Date('31 december 2020')).getTime() - date.getTime()) / 1000 / 60 / 60 /24);
+    date.setHours('0');
+    
+    function partOfDay(){
+        if (date.getHours() >= 4 && date.getHours() < 10) {return 'Доброе утро';}
+        if (date.getHours() >= 10 && date.getHours() < 17) {return 'Добрый день';}
+        if (date.getHours() >= 17 && date.getHours() < 21) {return 'Добрый вечер';}
+        if (date.getHours() >= 21 && date.getHours() < 24) {return 'Добрый ночи';}
+        if (date.getHours() >= 0 && date.getHours() < 10) {return 'Добрый ночи';}
+    }
+
+timeDays.textContent = partOfDay();
+week.textContent = weekTime[day];
+time.textContent = date.toLocaleTimeString('en');
+timeKol.textContent = count;
+
+console.log(date.getHours());
+
